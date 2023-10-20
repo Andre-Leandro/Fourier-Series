1254c3e72b4347c5809df13400463361134cdb25
SixtoArrejin <128509377+SixtoArrejin@users.noreply.github.com>
2023-10-20 15:32:21 -0300

    buenas practicas

--- fourierPotenciado.py
+++ fourierPotenciado.py
@@ -1,6 +1,8 @@
 from scipy.integrate import quad
 import numpy as np
 import matplotlib.pyplot as plt
+from ipywidgets import interact, widgets
+from IPython.display import display
 
 # La mitad del período
 l = 2.0
@@ -34,35 +36,17 @@ def bi(n):
     result = (1/l) * result
     return result
 
-# Imprime los resultados de los coeficientes
-print("a0:", a0)
-print("ai:", ai(1))
-print("bi:", bi)
-
 def serie_fourier(x, n, a0, ai, bi):
-    # Término constante (a0)
     suma = a0 / 2.0
-
-    # Suma para i desde 1 hasta n
     for i in range(1, n + 1):
         suma += ai(i) * np.cos((i/l)*np.pi*x) + bi(i) * np.sin((i/l)*np.pi*x)
-
     return suma
 
-# Crear una ventana emergente para ingresar el valor de n
-from tkinter.simpledialog import askinteger
-
-n = askinteger("Valor de n", "Ingrese el valor de n:")
-t = askinteger("Valor del periodo", "Ingrese el valor del periodo:")
-l = t/2
-
-# Verificar que se haya ingresado un valor válido para n
-if n is not None:
+def update_plot(n=15):
     x_values = np.linspace(-2*l, 2*l, 1000)
     y_values = [serie_fourier(x, n, a0, ai, bi) for x in x_values]
     y_original = [funcion(x) for x in x_values]
 
-    # Crear un gráfico
     plt.figure(figsize=(8, 6))
     plt.plot(x_values, y_values, label=f'Suma parcial (n={n})', color='b')
     plt.plot(x_values, y_original, label='Coseno 2', linestyle=':', color='r')
@@ -75,3 +59,9 @@ if n is not None:
     plt.axvline(0, color='black', linewidth=1)
     plt.xlim(-2*l, 2*l)
     plt.show()
+
+# Crea un widget interactivo para modificar n
+interact(update_plot, n=widgets.IntSlider(min=1, max=30, step=1, value=15))
+
+# Muestra el gráfico inicial
+update_plot(15)
