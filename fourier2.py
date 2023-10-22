b24897f3f7299e617f1f20e64f4deef70ad21ae3
SixtoArrejin <128509377+SixtoArrejin@users.noreply.github.com>
2023-10-22 16:28:16 -0300

    fourier2 presentable

--- fourier2.py
+++ fourier2.py
@@ -2,31 +2,19 @@ from scipy.integrate import quad
 import numpy as np
 import matplotlib.pyplot as plt
 
-# La mitad del período
-l = 2.0
+# Ingresar el valor de los coeficientes a0, an, bn y el valor de w0
+w0 = 1
+a0 = -np.pi/2
 
-# Define la función que deseas integrar
-def funcion(x):
-    if -2 <= x < 0:
-        return 0
-    elif 0 <= x < 2:
-        return 1
-    else:
-        return 0
-
-# Calcular a0
-result, error = quad(funcion, -l, l)
-a0 = 1
-
-# Calcular ai
+# Ingresar an
 def ai(n):
-    result = (2*np.sin((n * np.pi)/2)/(n * np.pi))
-    return result
+    a = (((-1)**n)-1)/((n**2)*np.pi)
+    return a
 
-# Calcular bi
+# Ingresar bn
 def bi(n):
-    result = ((1-2*((-1)**n)))/n
-    return 0
+    b = (1-(2*((-1)**n)))/n
+    return b
 
 # Imprime los resultados de los coeficientes
 print("a0:", a0)
@@ -39,27 +27,24 @@ def serie_fourier(x, n, a0, ai, bi):
 
     # Suma para i desde 1 hasta n
     for i in range(1, n + 1):
-        suma += ai(i) * np.cos(i*x*np.pi) + bi(i) * np.sin(i*x)
+        suma += ai(i) * np.cos(w0*i*x) + bi(i) * np.sin(w0*i*x)
     return suma
 
 # Crear una ventana emergente para ingresar el valor de n
 from tkinter.simpledialog import askinteger
 
 n = askinteger("Valor de n", "Ingrese el valor de n:")
-# t = askinteger("Valor del periodo", "Ingrese el valor del periodo:")
-t = 2
+t = 2*np.pi/w0
 l = t/2
 
 # Verificar que se haya ingresado un valor válido para n
 if n is not None:
     x_values = np.linspace(-2*l, 2*l, 1000)
     y_values = [serie_fourier(x, n, a0, ai, bi) for x in x_values]
-    # y_original = [funcion(x) for x in x_values]
 
     # Crear un gráfico
     plt.figure(figsize=(8, 6))
     plt.plot(x_values, y_values, label=f'Suma parcial (n={n})', color='b')
-    # plt.plot(x_values, y_original, label='Coseno 2', linestyle=':', color='r')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.title('Serie de Fourier')
